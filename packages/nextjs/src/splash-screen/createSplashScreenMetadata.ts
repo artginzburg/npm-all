f6998c3e39b1c93c1eb813d2ae788e21670f44a9
Art Ginzburg <ginzburg.arthur@gmail.com>
2025-05-29 23:23:17 +0400

    fix: urlBase in splash screens

--- packages/nextjs/src/splash-screen/createSplashScreenMetadata.ts
+++ packages/nextjs/src/splash-screen/createSplashScreenMetadata.ts
@@ -3,8 +3,8 @@ import { appleDeviceSizesWithSlugs } from './shared';
 import type { AppleWebApp } from 'next/dist/lib/metadata/types/extra-types';
 
 export function createSplashScreenMetadata(
-  /** @default './splash-screen/' */
-  urlBase = './splash-screen/',
+  /** @default '/splash-screen/' */
+  urlBase = '/splash-screen/',
 ): AppleWebApp['startupImage'] {
   return appleDeviceSizesWithSlugs.map(({ slug, width, height, pixelRatio }) => ({
     url: `${urlBase}${slug}`,
