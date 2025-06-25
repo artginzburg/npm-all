501829bab0a60802e493102ea4159a3d7e4352eb
Art Ginzburg <ginzburg.arthur@gmail.com>
2025-06-25 16:46:10 +0400

    chore: Update ESLint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,9 +2,10 @@ import eslint from '@eslint/js';
 import pluginImport from 'eslint-plugin-import-x';
 import tseslint from 'typescript-eslint';
 
-export default tseslint.config(
+/** @type {import('typescript-eslint').ConfigArray} */
+const config = tseslint.config(
   eslint.configs.recommended,
-  ...tseslint.configs.strict,
+  tseslint.configs.strict,
   {
     ignores: ['**/lib'],
   },
@@ -39,3 +40,5 @@ export default tseslint.config(
     },
   },
 );
+
+export default config;
